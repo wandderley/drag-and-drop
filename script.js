3a14469af43e1a7a4ef1077a7a1f53fb2459b013
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-23 00:16:49 -0300

    feat: função para criar cards e editá-los

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let draggedCard = null;
 // Função Cards
 const dragStart = (event) => {
     draggedCard = event.target;
-    event.dataTransfer.effectAllowed = "move"
+    event.dataTransfer.effectAllowed = "move";
 };
 
 // Função coluna
@@ -17,29 +17,54 @@ const dragOver = (event) => {
 
 const dragEnter = ({target}) => {
     if(target.classList.contains("column_cards")){
-        target.classList.add("column_highlight")
+        target.classList.add("column_highlight");
     }
 };
 
 const dragLeave = ({target}) => {
-    target.classList.remove("column_highlight")    
+    target.classList.remove("column_highlight")    ;
 };
 
 // Função drop
 const drop = ({target}) => {
     if(target.classList.contains("column_cards")){
-        target.classList.remove("column_highlight")
-        target.append(draggedCard)
+        target.classList.remove("column_highlight");        target.append(draggedCard);
     }
+};
+
+const createCard = ({target}) => {
+    if(!target.classList.contains("column_cards")) return
+
+    const card = document.createElement("section");
+
+    card.className = "card";
+    card.draggable = "true";
+    card.contentEditable = "true"
+
+    card.addEventListener("focusout", () => {
+        card.contentEditable = "false";
+        if(!card.textContent){
+            card.remove();
+        }
+    })
+
+    card.addEventListener("dragstart", dragStart);
+
+
+    target.append(card)
+    card.focus()
+
 };
 
 
 // Evento de capura para as colunas
 columns.forEach((column) => {
-    column.addEventListener("dragover", dragOver) // Está sobre
-    column.addEventListener("dragenter", dragEnter ) // Entra
-    column.addEventListener("dragleave", dragLeave ) // Arrasta pra fora
-    column.addEventListener("drop", drop ) // Arrasta pra fora
+    column.addEventListener("dragover", dragOver); // Está sobre
+    column.addEventListener("dragenter", dragEnter ) ;// Entra
+    column.addEventListener("dragleave", dragLeave ); // Arrasta pra fora
+    column.addEventListener("drop", drop ); // solta
+    column.addEventListener("dblclick", createCard ); // adiciona novo card
+
 })
 
 // Evento de capura para os cards
